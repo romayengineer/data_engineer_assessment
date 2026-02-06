0c3bbc412d4aefcf517dad64912f797b1ed9f4e2
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-02-06 13:35:52 -0300

    campania

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 
 # Random helper data
-campañas = ["2023/24", "2024/25", "2025/26"]
+campanias = ["2023/24", "2024/25", "2025/26"]
 
 # Lote IDs
 lote_ids = ["L000011", "L020031", "L021003", "L033022"]
@@ -20,7 +20,7 @@ def generate_clima_diario(num_rows):
     filename = "data/clima_diario.csv"
 
     # Column headers
-    columns = ["lote_id", "campaña", "fecha", "temp_min_c", "temp_max_c", "precipitacion_mm"]
+    columns = ["lote_id", "campania", "fecha", "temp_min_c", "temp_max_c", "precipitacion_mm"]
 
     with open(filename, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
@@ -28,7 +28,7 @@ def generate_clima_diario(num_rows):
 
         for i in range(1, num_rows + 1):
             lote_id = random.choice(lote_ids)
-            campaña = random.choice(campañas)
+            campania = random.choice(campanias)
             fecha = (start_date + timedelta(days=i)).strftime("%Y-%m-%d")
 
             temp_min = round(random.uniform(5, 20), 1)
@@ -36,7 +36,7 @@ def generate_clima_diario(num_rows):
 
             precipitacion = round(random.uniform(0, 50), 1)
 
-            writer.writerow([lote_id, campaña, fecha, temp_min, temp_max, precipitacion])
+            writer.writerow([lote_id, campania, fecha, temp_min, temp_max, precipitacion])
 
     print(f"new CSV: {filename}")
 
@@ -48,7 +48,7 @@ def generate_rinde_lotes(num_rows):
     cultivos = ["Soja", "Maiz", "Trigo"]
 
     # Column headers
-    columns = ["lote_id", "campaña", "fecha", "cultivo", "superficie_ha", "rinde_kg_ha", "humedad_pct"]
+    columns = ["lote_id", "campania", "fecha", "cultivo", "superficie_ha", "rinde_kg_ha", "humedad_pct"]
 
     with open(filename, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
@@ -56,7 +56,7 @@ def generate_rinde_lotes(num_rows):
 
         for i in range(1, num_rows + 1):
             lote_id = random.choice(lote_ids)
-            campaña = random.choice(campañas)
+            campania = random.choice(campanias)
             fecha = (start_date + timedelta(days=i)).strftime("%Y-%m-%d")
 
             cultivo = random.choice(cultivos)
@@ -67,7 +67,7 @@ def generate_rinde_lotes(num_rows):
 
             humedad_pct = round(random.uniform(10, 15), 2)
 
-            writer.writerow([lote_id, campaña, fecha, cultivo, superficie_ha, rinde_kg_ha, humedad_pct])
+            writer.writerow([lote_id, campania, fecha, cultivo, superficie_ha, rinde_kg_ha, humedad_pct])
 
     print(f"new CSV: {filename}")
 
